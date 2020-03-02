e6991c7443b0e1d8b315ab8c6ad7edf8912f817e
Ranjith <email@email.com>
2020-03-02 14:15:56 +0530

    first commit

---
+++ src/test/java/SeleniumFramework/AppiumTest.java
@@ -0,0 +1,18 @@
+package SeleniumFramework;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void Android()
+	{
+		System.out.println("Android");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
